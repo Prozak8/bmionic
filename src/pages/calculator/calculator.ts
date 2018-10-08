c0bd7b01c5070a3867621d33d8d6942c0ad325f3
Zak <zakary.howard94@gmail.com>
2018-10-08 14:49:26 +0200

    html form created, requiring logic to work

--- src/pages/calculator/calculator.ts
+++ src/pages/calculator/calculator.ts
@@ -1,21 +1,20 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the CalculatorPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-calculator',
   templateUrl: 'calculator.html',
 })
 export class CalculatorPage {
+  height: number;
+  weight: number;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  calculateBMI() {
+    
   }
 
   ionViewDidLoad() {
