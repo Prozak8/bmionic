30db30f4105af9013d17f7766273c0a587dfb02d
Zak <zakary.howard94@gmail.com>
2018-12-27 22:31:47 +0100

    calculator page successfully being created

--- src/pages/calculator/calculator.spec.ts
+++ src/pages/calculator/calculator.spec.ts
@@ -1,9 +1,9 @@
 import { CalculatorPage } from "./calculator";
 import { TestBed, async } from "@angular/core/testing";
-import { IonicModule, Platform, NavController } from "ionic-angular";
+import { IonicModule, Platform, NavController, NavParams } from "ionic-angular";
 import { StatusBar } from "@ionic-native/status-bar";
 import { SplashScreen } from "@ionic-native/splash-screen";
-import { PlatformMock, StatusBarMock, SplashScreenMock, NavControllerMock } from "ionic-mocks";
+import { PlatformMock, StatusBarMock, SplashScreenMock, NavControllerMock, NavParamsMock } from "ionic-mocks";
 
 describe("CalculatorPage", () => {
   let calculator;
@@ -19,7 +19,8 @@ describe("CalculatorPage", () => {
         { provide: Platform, useFactory: () => PlatformMock.instance() },
         { provide: StatusBar, useFactory: () => StatusBarMock.instance() },
         { provide: SplashScreen, useFactory: () => SplashScreenMock.instance() },
-        { provide: NavController, useFactory: () => NavControllerMock.instance() }
+        { provide: NavController, useFactory: () => NavControllerMock.instance() },
+        { provide: NavParams, useFactory: () => NavParamsMock.instance() }
       ]
     }).compileComponents();
   }));
